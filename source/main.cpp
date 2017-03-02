0e278128836bed61f8c16cf3422d0efd70507bb2
KotoriCANOE <sanma.kote@gmail.com>
2017-03-02 01:36:07 +0000

    Added Bailian<2813> and Bailian<4144>

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,15 @@
 ////////////////////////////////////////////////////////////////
 
+#if defined (__GNUC__)
+#pragma GCC optimize (2)
+#pragma GCC diagnostic error "-fopenmp -lpthread"
+#endif
+
 #include <array>
 #include <vector>
+#include <forward_list>
 #include <string>
+#include <cstring>
 #include <random>
 #include <cmath>
 #include <algorithm>
@@ -13,6 +20,7 @@
 ////////////////////////////////////////////////////////////////
 
 //#define DEBUG
+#define __SSE2__
 //#define __SSE4_1__
 
 #ifdef _MSC_VER
@@ -20,15 +28,8 @@
 #endif
 
 ////////////////////////////////////////////////////////////////
-
-typedef std::chrono::high_resolution_clock MyClock;
-typedef std::chrono::duration<double> MySeconds;
-typedef std::chrono::duration<double, std::milli> MyMilliseconds;
-typedef std::chrono::duration<double, std::micro> MyMicroseconds;
-
-////////////////////////////////////////////////////////////////
-
 // Intrinsics
+
 #if defined(__AVX2__) || defined(__AVX__)
 #include <immintrin.h>
 #elif defined(__SSE4_2__)
@@ -138,20 +139,108 @@ static inline __m128i operator/(const __m128i &a, const Divisor_us &d) {
 }
 
 ////////////////////////////////////////////////////////////////
-
 // OpenMP
+
 #ifdef _OPENMP
 #include <omp.h>
 #endif
+
+////////////////////////////////////////////////////////////////
+// Type definitions
+
+typedef std::chrono::high_resolution_clock MyClock;
+typedef std::chrono::duration<double> MySeconds;
+typedef std::chrono::duration<double, std::milli> MyMilliseconds;
+typedef std::chrono::duration<double, std::micro> MyMicroseconds;
+
+////////////////////////////////////////////////////////////////
+// Helper classes
+
+class MyTimer
+{
+public:
+    typedef MyTimer _Myt;
+    typedef std::chrono::high_resolution_clock Clock;
+    typedef Clock::time_point TimePoint;
+    typedef std::chrono::duration<double> Seconds;
+    typedef std::chrono::duration<double, std::milli> Milliseconds;
+    typedef std::chrono::duration<double, std::micro> Microseconds;
+    typedef std::chrono::duration<double, std::nano> Nanoseconds;
+
+private:
+    TimePoint t_start, t_end;
+
+public:
+    MyTimer()
+        : t_start(Clock::now()), t_end(TimePoint())
+    {}
+
+    TimePoint Start() noexcept
+    {
+        t_end = TimePoint();
+        t_start = Clock::now();
+        return t_start;
+    }
+
+    TimePoint End() noexcept
+    {
+        t_end = Clock::now();
+        return t_end;
+    }
+
+    template <typename _Duration>
+    _Duration Duration() noexcept
+    {
+        if (t_end == TimePoint()) End();
+        return std::chrono::duration_cast<_Duration>(t_end - t_start);
+    }
+
+    template <typename _Duration>
+    _Myt &Print(const std::string &title = "Timer", double division = 1) noexcept
+    {
+        auto time_span = std::chrono::duration_cast<_Duration>(Duration<_Duration>()).count();
+        if (division != 1) time_span /= division;
+        std::cout << title << ": It took " << time_span << " " << getUnit<_Duration>() << ".\n";
+        return *this;
+    }
+
+private:
+    template <typename _Duration>
+    std::string getUnit() noexcept
+    {
+        std::string unit;
+        if (std::is_same<_Duration, Seconds>::value)
+            return "seconds";
+        else if (std::is_same<_Duration, Milliseconds>::value)
+            return "milliseconds";
+        else if (std::is_same<_Duration, Microseconds>::value)
+            return "microseconds";
+        else if (std::is_same<_Duration, Nanoseconds>::value)
+            return "nanoseconds";
+        else
+            return "<unknown unit>";
+    }
+};
+
+////////////////////////////////////////////////////////////////
+// Helper functions
+
+template <typename _Ty>
+static inline _Ty DivideRoundUp(const _Ty &a, const _Ty &b)
+{
+    static_assert(std::is_integral<_Ty>::value, "DivideRoundUp: only integer input is accepted");
+    return (a + b - 1) / b;
+}
 
 ////////////////////////////////////////////////////////////////
 
 template <int _ID>
 struct Bailian
 {
-    void operator()() noexcept {}
+    static void main() noexcept {}
 };
 
+// http://bailian.openjudge.cn/practice/2774/
 template <>
 struct Bailian<2774>
 {
@@ -194,7 +283,7 @@ struct Bailian<2774>
         }
 
 #ifdef DEBUG
-        MyClock::time_point t1 = MyClock::now();
+        MyTimer timer;
         const int loops = 10000;
 
         for (int loop = 0; loop < loops; ++loop)
@@ -214,9 +303,8 @@ struct Bailian<2774>
                 __m128i wLengthSum_v = _mm_setzero_si128(); // epi32
                 __m128i wLengthMin_v = _mm_set1_epi16(static_cast<uint16_t>(wLengthMin)); // epu16
 
-                static const ptrdiff_t simd_step = 8;
-                const ptrdiff_t simd_residue = N % simd_step;
-                const ptrdiff_t simd_N = N - simd_residue;
+                constexpr const ptrdiff_t simd_step = 8;
+                const ptrdiff_t simd_N = N - N % simd_step;
 
                 for (const T *upper = wLengthsPtr + simd_N; wLengthsPtr < upper; wLengthsPtr += simd_step)
                 {
@@ -263,7 +351,7 @@ struct Bailian<2774>
                 Divisor_us sLength_divisor(sLength);
                 __m128i segments_v = _mm_setzero_si128();
 
-                static const ptrdiff_t simd_step = 8;
+                constexpr const ptrdiff_t simd_step = 8;
                 const ptrdiff_t simd_residue = N % simd_step;
                 const ptrdiff_t simd_N = N - simd_residue;
 
@@ -286,14 +374,384 @@ struct Bailian<2774>
             }
         } while (0);
 #ifdef DEBUG
-        MySeconds time_span = std::chrono::duration_cast<MySeconds>(MyClock::now() - t1);
-
-        std::cout << ": It took "
-            << std::chrono::duration_cast<MyMicroseconds>(time_span).count() / loops
-            << " microseconds.\n";
+        timer.Print<MyTimer::Microseconds>("1", loops);
 #endif
 
-        printf("%d\n", sLengthResult);
+        printf("%d", sLengthResult);
+    }
+};
+
+// http://bailian.openjudge.cn/practice/2813/
+template <>
+struct Bailian<2813>
+{
+    typedef uint32_t T;
+    static const int MaxIter = 0x10;
+
+    static void main() noexcept
+    {
+        // Input
+        int n;
+        scanf("%d", &n);
+
+        const int height = n;
+        const int width = n;
+        const int padding = 1;
+        const int simd_step = static_cast<int>(sizeof(__m128i) / sizeof(T));
+        const int simd_width = DivideRoundUp(width, simd_step) * simd_step;
+        const int stride = simd_width + padding * 2;
+        const int size = (n + padding * 2) * stride + padding;
+        std::vector<T> wallData(size, UINT32_MAX); // padded wall
+        auto *wallDataBegin = wallData.data() + stride + 1; // pointer of wall's origin
+
+        for (int j = 0; j < height; ++j)
+        {
+            auto *datap = wallDataBegin + j * stride;
+
+            for (int i = 0; i < width; ++i)
+            {
+                while (int c = getchar())
+                {
+                    if (c == 'w')
+                    {
+                        *datap = 0;
+                        break;
+                    }
+                    else if (c == 'y')
+                    {
+                        *datap = UINT32_MAX;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Recursion implementation
+        int steps = implement1(0, wallData, height, width, stride, simd_width, simd_step);
+
+        if (steps > MaxIter) printf("inf\n");
+        else printf("%d\n", steps);
+    }
+
+protected:
+    // Start from the Largest possible steps
+    // Randomly create drawing patterns and verify if they meet the requirement
+    // O(Choose(n*n, n*n/2)) = O((n*n)! / (n*n/2)!^2)
+    static int implement2(const std::vector<T> &wallData, int height, int width, int stride)
+    {
+        const int number = height * width;
+
+    }
+
+    // Recursive implementation of exhaustive search
+    // Not applicable, and can go repeated steps on the same bricks, which should not happen.
+    // O((n*n) ^ MaxIter)
+    static int implement1(int recursionTimes, const std::vector<T> &lastWallData,
+        int height, int width, int stride, int simd_width, int simd_step)
+    {
+        // Prepare copyed data
+        auto wallData = lastWallData;
+        auto *wallDataBegin = wallData.data() + stride + 1;
+
+        // Set outer border to UINT32_MAX
+        // (for those should always be UINT32_MAX, but could possibly be inverted)
+        { // j = -1
+            auto *datap = wallData.data();
+            for (const auto *upper = datap + width + 2; datap < upper; ++datap)
+            {
+                *datap = UINT32_MAX;
+            }
+        }
+        for (int j = 0; j < height; ++j)
+        {
+            auto *datap = wallData.data() + j * stride;
+            datap[0] = UINT32_MAX;
+            datap[width + 1] = UINT32_MAX;
+        }
+        { // j = width
+            auto *datap = wallData.data() + (width + 1) * stride;
+            for (const auto *upper = datap + width + 2; datap < upper; ++datap)
+            {
+                *datap = UINT32_MAX;
+            }
+        }
+
+        // Determine if the requirement is met
+        T sign = UINT32_MAX;
+#if defined(__SSE2__)
+        __m128i sign_v = _mm_set1_epi32(sign);
+#endif
+
+        for (int j = 0; j < height; ++j)
+        {
+            const auto *srcp = wallDataBegin + j * stride;
+
+#if defined(__SSE2__)
+            for (const auto *upper = srcp + simd_width; srcp < upper; srcp += simd_step)
+            {
+                sign_v = _mm_and_si128(sign_v, _mm_loadu_si128(reinterpret_cast<const __m128i *>(srcp)));
+            }
+#endif
+            for (const auto *upper = srcp + width; srcp < upper; ++srcp)
+            {
+                sign &= *srcp;
+            }
+        }
+
+#if defined(__SSE2__)
+        alignas(sizeof(sign_v)) decltype(sign) sign_va[sizeof(sign_v) / sizeof(sign)];
+        _mm_store_si128(reinterpret_cast<__m128i *>(sign_va), sign_v);
+        sign &= sign_va[0] & sign_va[1] & sign_va[2] & sign_va[3];
+#endif
+
+        if (sign) return recursionTimes; // terminate recursion when all the bricks are yellow
+
+        // Terminate recursion when exceeding iteration limitation
+        if (++recursionTimes > MaxIter) return INT32_MAX;
+
+        // Apply erosion morphological filter
+        std::vector<T> wallEroded(wallData.size());
+        auto *wallErodedBegin = wallEroded.data() + stride + 1;
+
+        for (int j = 0; j < height; ++j)
+        {
+            const auto *srcp = wallDataBegin + j * stride;
+            auto *dstp = wallErodedBegin + j * stride;
+
+#if defined(__SSE2__)
+            for (const auto *upper = dstp + simd_width; dstp < upper; dstp += simd_step, srcp += simd_step)
+            {
+                const __m128i s0 = _mm_loadu_si128(reinterpret_cast<const __m128i *>(srcp - stride));
+                const __m128i s1 = _mm_loadu_si128(reinterpret_cast<const __m128i *>(srcp - 1));
+                const __m128i s2 = _mm_loadu_si128(reinterpret_cast<const __m128i *>(srcp));
+                const __m128i s3 = _mm_loadu_si128(reinterpret_cast<const __m128i *>(srcp + 1));
+                const __m128i s4 = _mm_loadu_si128(reinterpret_cast<const __m128i *>(srcp + stride));
+
+                const __m128i d = _mm_and_si128(s2, _mm_and_si128(_mm_and_si128(s0, s4), _mm_and_si128(s1, s3)));
+                _mm_storeu_si128(reinterpret_cast<__m128i *>(dstp), d);
+            }
+#endif
+            for (const auto *upper = dstp + width; dstp < upper; ++dstp, ++srcp)
+            {
+                *dstp = srcp[0] & (srcp[-stride] & srcp[stride]) & (srcp[-1] & srcp[1]);
+            }
+        }
+
+        // Recursive searches on possible positions
+        constexpr const T inv_mask = UINT32_MAX;
+        const __m128i inv_mask_v = _mm_setr_epi32(inv_mask, inv_mask, inv_mask, 0);
+        int minRecursionTimes = INT32_MAX;
+
+        for (int j = 0; j < height; ++j)
+        {
+            const auto *erodep = wallErodedBegin + j * stride;
+            auto *data1p = wallDataBegin + j * stride - 1;
+            auto *data0p = data1p - stride + 1;
+            auto *data2p = data1p + stride + 1;
+
+            for (const auto *upper = erodep + width; erodep < upper && *erodep == 0;
+                ++erodep, ++data0p, ++data1p, ++data2p)
+            {
+                *data0p ^= inv_mask;
+                *data2p ^= inv_mask;
+                _mm_storeu_si128(reinterpret_cast<__m128i *>(data1p),
+                    _mm_xor_si128(_mm_loadu_si128(reinterpret_cast<const __m128i *>(data1p)), inv_mask_v));
+
+                const int recurs = implement1(recursionTimes, wallData, height, width, stride, simd_width, simd_step);
+                if (recurs < minRecursionTimes) minRecursionTimes = recurs;
+
+                *data0p ^= inv_mask;
+                *data2p ^= inv_mask;
+                _mm_storeu_si128(reinterpret_cast<__m128i *>(data1p),
+                    _mm_xor_si128(_mm_loadu_si128(reinterpret_cast<const __m128i *>(data1p)), inv_mask_v));
+            }
+        }
+
+        return minRecursionTimes;
+    }
+
+    // code by someone others
+    static int other1()
+    {
+        using namespace std;
+
+        int m, n, i, j, a[20], t[20], min, cpy[20];
+        char c;
+        scanf("%d", &m);
+        while (m--)
+        {
+            scanf("%d", &n);
+            memset(a, 0, sizeof(int) * 20);
+            memset(t, 0, sizeof(int) * 20);
+            memset(cpy, 0, sizeof(int) * 20);
+            min = 10000;
+            for (i = 0; i<n; i++)
+                for (j = 0; j<n; j++)
+                    if (cin >> c, c == 'w')
+                        a[i] ^= (1 << j);
+            memcpy(cpy, a, sizeof(int) * 20);
+            for (int k = 0; k<(1 << n); k++)
+            {
+                memcpy(a, cpy, sizeof(int) * 20);
+                memset(t, 0, sizeof(int) * 20);
+                for (i = 0; i<n; i++)
+                {
+                    if (i == 0)
+                        t[i] = k;
+                    else
+                        t[i] = a[i - 1];
+                    a[i] ^= t[i];
+                    a[i] ^= t[i] >> 1;
+                    a[i] ^= (t[i] << 1) & ((1 << n) - 1);
+                    a[i + 1] ^= t[i];
+                }
+                if (a[n - 1] == 0)
+                {
+                    int tem = 0;
+                    for (i = 0; i<n; i++)
+                        while (t[i])
+                        {
+                            t[i] &= (t[i] - 1);
+                            tem++;
+                        }
+                    if (tem < min)
+                        min = tem;
+                }
+            }
+            if (min == 10000)
+                printf("inf\n");
+            else
+                printf("%d\n", min);
+
+        }
+        system("pause");
+        return 0;
+    }
+};
+
+// http://bailian.openjudge.cn/practice/4144/
+template <>
+struct Bailian<4144>
+{
+    typedef std::array<int, 4> MyArray; // { cowNum [0...], A, B, stableNum [1...] }
+    typedef std::vector<MyArray> MyVector;
+    typedef std::forward_list<MyArray> MyList;
+
+    static const int TIMEPOINT_MIN = 1;
+    static const int TIMEPOINT_MAX = 1000000;
+    static const int DURATION_MIN = 0;
+    static const int DURATION_MAX = 500000;
+
+    static void main()
+    {
+        // Input
+#ifdef DEBUG
+        MyTimer timer;
+
+        unsigned int seed = static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count());
+        std::default_random_engine generator(seed);
+        std::uniform_int_distribution<int> distribution1(TIMEPOINT_MIN, TIMEPOINT_MAX);
+        std::uniform_int_distribution<int> distribution2(DURATION_MIN, DURATION_MAX);
+
+        int N = 50000;
+
+        MyVector cowVec(N);
+        for (int i = 0; i < N; ++i)
+        {
+            MyArray &cow = cowVec[i];
+            cow[0] = i;
+            cow[1] = distribution1(generator);
+            cow[2] = std::min(TIMEPOINT_MAX, cow[1] + distribution2(generator));
+        }
+
+        timer.Start();
+#else
+        int N;
+        scanf("%d", &N);
+
+        MyVector cowVec(N);
+        for (int i = 0; i < N; ++i)
+        {
+            MyArray &cow = cowVec[i];
+            cow[0] = i;
+            scanf("%d %d", &cow[1], &cow[2]);
+        }
+#endif
+        // Implement
+        std::sort(cowVec.begin(), cowVec.end(), [](const MyArray &left, const MyArray &right)
+        {
+            return left[1] < right[1] || (left[1] == right[1] && left[2] > right[2]);
+        });
+
+        MyList cowList1(N);
+        MyList cowList2;
+        std::copy(cowVec.begin(), cowVec.end(), cowList1.begin());
+
+#ifdef DEBUG
+        timer.Print<MyTimer::Milliseconds>("1");
+        timer.Start();
+#endif
+
+        int stableNum = 0;
+
+        while (!cowList1.empty())
+        {
+            ++stableNum;
+            int last = 0;
+
+            auto cowIter1 = cowList1.begin();
+            auto lastCowIter1 = cowList1.before_begin();
+
+            for(; cowIter1 != cowList1.end();)
+            {
+                MyArray &cow = *cowIter1;
+
+                if (cow[1] > last)
+                {
+                    last = cow[2];
+                    cow[3] = stableNum;
+                    cowList2.push_front(std::move(cow));
+                    cowIter1 = cowList1.erase_after(lastCowIter1);
+                }
+                else
+                {
+                    ++cowIter1;
+                    ++lastCowIter1;
+                }
+            }
+        }
+
+#ifdef DEBUG
+        timer.Print<MyTimer::Milliseconds>("2");
+        timer.Start();
+#endif
+
+        // Output
+        std::copy(cowList2.begin(), cowList2.end(), cowVec.begin());
+        std::sort(cowVec.begin(), cowVec.end(), [](const MyArray &left, const MyArray &right)
+        {
+            return left[0] < right[0];
+        });
+
+#ifdef DEBUG
+        timer.Print<MyTimer::Milliseconds>("3");
+        timer.Start();
+#endif
+
+        std::string outStr;
+        outStr.reserve(N * 7);
+        outStr.append(std::to_string(stableNum)).append("\n");
+
+        std::for_each(cowVec.begin(), cowVec.end(), [&](const MyArray &cow)
+        {
+            outStr.append(std::to_string(cow[3])).append("\n");
+        });
+
+#ifdef DEBUG
+        timer.Print<MyTimer::Milliseconds>("4");
+#else
+        printf(outStr.c_str());
+#endif
     }
 };
 
@@ -301,7 +759,7 @@ struct Bailian<2774>
 
 int main(int argc, char **argv)
 {
-    Bailian<2774>::main();
+    Bailian<4144>::main();
 
 #ifdef DEBUG
     getchar();
